b2ef8e04549d872d0768ccb0c7d4a7cd538e96e0
Toliak <ibolosik@gmail.com>
2019-06-11 20:47:51 +0300

    Added mtasa server config and testing resource

--- tests/mtasa_server/mods/deathmatch/resources/test/moduleTest.lua
+++ tests/mtasa_server/mods/deathmatch/resources/test/moduleTest.lua
@@ -1,3 +1,18 @@
+local TEST_FUNCTIONS = {
+    {
+        name = "moduleSdkTest1",
+        expected = {"Yesn't!"}
+    },
+}
+
 addEventHandler("onResourceStart", resourceRoot, function()
-    iprint("Yep!")
+    for _, v in pairs(TEST_FUNCTIONS) do
+        addTest(v.name)
+    end
+
+    for _, v in pairs(TEST_FUNCTIONS) do
+        runTest(v.name, v.expected)
+    end
+
+    testStatus()
 end)
